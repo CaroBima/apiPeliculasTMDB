619c080c3252d47066067a037f3c2d17cc49560f
CaroBima <carobima@gmail.com>
2023-10-09 18:20:44 -0300

    Configuracion de la app

--- src/main/java/com/peliculas/tmdbapi/TmdbapiApplication.java
+++ src/main/java/com/peliculas/tmdbapi/TmdbapiApplication.java
@@ -1,13 +1,27 @@
 package com.peliculas.tmdbapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class TmdbapiApplication {
 
+	@Value("${tmdb.api.key}")
+	private static String tmdbApiKey;
 	public static void main(String[] args) {
 		SpringApplication.run(TmdbapiApplication.class, args);
+		String valor = System.getenv("APIKEYPELICULASTMDB");
+		System.out.println(tmdbApiKey);
+		System.out.println(valor);
+
+		Map<String, String> env = System.getenv();
+		for (String key : env.keySet()) {
+			System.out.println(key + ": " + env.get(key));
+		}
+
 	}
 
 }
