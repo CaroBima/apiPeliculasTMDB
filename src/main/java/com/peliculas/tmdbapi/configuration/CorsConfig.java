7d28c84edfed51841c1f0f9c2a65afa015a32de3
CaroBima <carobima@gmail.com>
2023-10-13 07:04:32 -0300

    Cambiando javadoc de CorsConfig para que sea mas completo

--- src/main/java/com/peliculas/tmdbapi/configuration/CorsConfig.java
+++ src/main/java/com/peliculas/tmdbapi/configuration/CorsConfig.java
@@ -7,11 +7,24 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * Clase que permite la configuración de los Cors
+ * Configuración de Cross-Origin Resource Sharing (CORS) para permitir solicitudes
+ * desde un origen específico (http://localhost:4200 en este caso).
+ *
+ * Esta configuración utiliza la anotación {@link Configuration} y proporciona un bean
+ * de tipo {@link WebMvcConfigurer} para configurar políticas CORS en la aplicación.
+ *
+ * @see WebMvcConfigurer
  */
 @Getter
 @Configuration
 public class CorsConfig {
+
+    /**
+     * Bean que configura las políticas CORS para permitir solicitudes desde
+     * http://localhost:4200 y especifica los métodos permitidos (GET, POST, PUT, DELETE).
+     *
+     * @return Instancia de {@link WebMvcConfigurer} con la configuración CORS.
+     */
     @Bean
     public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
