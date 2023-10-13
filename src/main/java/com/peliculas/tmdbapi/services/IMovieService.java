92f503133c2e8e4dcd8ef41f3da4b5b8e53cb19c
CaroBima <carobima@gmail.com>
2023-10-13 06:34:33 -0300

    Documentando y agregando comentarios

--- src/main/java/com/peliculas/tmdbapi/services/IMovieService.java
+++ src/main/java/com/peliculas/tmdbapi/services/IMovieService.java
@@ -13,8 +13,26 @@ import java.util.List;
  */
 
 public interface IMovieService {
+    /**
+     * Obtiene una película por su título.
+     *
+     * @param title Título de la película que se desea obtener.
+     * @return La película correspondiente al título especificado, o {@code null} si no se encuentra.
+     */
     public Movie getMovie(String title);
+
+    /**
+     * Permite guardar una película en la base de datos.
+     *
+     * @param movie La película que se va a guardar.
+     */
     public void saveMovie(Movie movie);
+
+    /**
+     * Obtiene la lista de películas guardadas en la base de datos.
+     *
+     * @return Lista de películas almacenadas en la base de datos.
+     */
     public List<Movie> getMoviesSaved();
 
 
--- src/main/java/com/peliculas/tmdbapi/services/MovieService.java
+++ src/main/java/com/peliculas/tmdbapi/services/MovieService.java
@@ -5,20 +5,42 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Implementación de la interfaz {@link IMovieService} que proporciona lógica
+ * para interactuar con películas en un servicio.
+ *
+ * @see IMovieService
+ * @see Movie
+ */
 @Service
 public class MovieService implements  IMovieService{
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Movie getMovie(String title) {
+        // La implementación de la lógica para obtener una película por su título
+        // aún no está definida. Se devuelve null temporalmente.
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void saveMovie(Movie movie) {
-
+        // La implementación de la lógica para guardar una película
+        // aún no está definida.
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Movie> getMoviesSaved() {
+        // La implementación de la lógica para obtener la lista de películas
+        // guardadas aún no está definida. Se devuelve null temporalmente.
         return null;
     }
 }
