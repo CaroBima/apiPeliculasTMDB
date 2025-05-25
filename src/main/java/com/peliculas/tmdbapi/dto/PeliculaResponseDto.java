4ab5c85e47c12d585f0df70c21a7b0b0e37f4fd3
Carolina Bima <carolina.bima@ayi.group>
2025-05-24 23:55:43 -0300

    PE001 Add devolucion listado de novedades (closes #1)

---
+++ src/main/java/com/peliculas/tmdbapi/dto/PeliculaResponseDto.java
@@ -0,0 +1,37 @@
+package com.peliculas.tmdbapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PeliculaResponseDto {
+    private Long id;
+    private String title;
+
+    @JsonProperty("original_title")
+    private String originalTitle;
+
+    private String overview;
+
+    @JsonProperty("release_date")
+    private Date releaseDate;
+
+    @JsonProperty("poster_path")
+    private String posterPath;
+
+    @JsonProperty("vote_average")
+    private Double voteAverage;
+
+    @JsonProperty("vote_count")
+    private Long voteCount;
+}
